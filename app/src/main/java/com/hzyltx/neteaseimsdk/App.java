78bc36dee2d565a957398761ec283b722c0550f4
hzyltx <3059485516@qq.com>
2021-07-14 15:46:46 +0800

    修改已知问题

--- app/src/main/java/com/hzyltx/neteaseimsdk/App.java
+++ app/src/main/java/com/hzyltx/neteaseimsdk/App.java
@@ -2,6 +2,8 @@ package com.hzyltx.neteaseimsdk;
 
 import android.support.multidex.MultiDexApplication;
 
+import com.netease.nim.yl.NeteaseApp;
+
 /**
  * @author Yang Shihao
  */
@@ -10,5 +12,6 @@ public class App extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+        NeteaseApp.neteaseIMInit(this,"",MainActivity.class,MainActivity.class);
     }
 }
--- app/src/main/java/com/hzyltx/neteaseimsdk/MainActivity.java
+++ app/src/main/java/com/hzyltx/neteaseimsdk/MainActivity.java
@@ -1,13 +1,30 @@
 package com.hzyltx.neteaseimsdk;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.bt);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jump();
+            }
+        });
+    }
+
+    private void jump(){
+        Intent intent = new Intent(this, com.netease.nim.yl.main.activity.MainActivity.class);
+        startActivity(intent);
     }
 }
